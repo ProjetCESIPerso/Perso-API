3b33614e50e2d16c8385e11962406c10cd11bcbb
camille <camille.melkebeek@viacesi.fr>
2023-11-12 00:25:27 +0100

    Ajout génération 1000 user

---
+++ AnnuaireEntrepriseAPI/DataGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using AnnuaireEntrepriseAPI.Database;
+using AnnuaireEntrepriseAPI.DTOs;
+using AnnuaireEntrepriseAPI.Models;
+using Bogus;
+
+namespace AnnuaireEntrepriseAPI
+{
+    public class DataGenerator
+    {
+        private readonly AnnuaireEntrepriseContext _context;
+
+        public DataGenerator(AnnuaireEntrepriseContext context)
+        {
+            _context = context;
+        }
+
+        public void GenerateData(AnnuaireEntrepriseContext dataContext)
+        {
+            var faker = new Faker("fr");
+
+            var random = new Random();
+
+            var employees = new List<User>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var mobilePhone = Convert.ToInt32(GenerateRandomPhoneNumber(random));
+                var phoneNumber = Convert.ToInt32(GenerateRandomPhoneNumber(random));
+
+                var employee = new User
+                {
+                    Name = faker.Name.LastName(),
+                    Surname = faker.Name.FirstName(),
+                    MobilePhone = mobilePhone.ToString(),
+                    PhoneNumber = phoneNumber.ToString(),
+                    Email = faker.Internet.Email(),
+                    Service = GetRandomService(random),
+                    Site = GetRandomSite(random)
+
+                };
+
+                employees.Add(employee);
+            }
+
+            dataContext.Users.AddRange(employees);
+            dataContext.SaveChanges();
+
+            Console.WriteLine("Insertion des données terminée !");
+        }
+
+        private string GenerateRandomPhoneNumber(Random random)
+        {
+            var phoneNumber = "0";
+
+            for (int i = 0; i < 9; i++)
+            {
+                phoneNumber += random.Next(0, 10);
+            }
+
+            return phoneNumber;
+        }
+
+        private Service GetRandomService(Random random)
+        {
+            List<Service> services = _context.Services.ToList();
+
+
+            return services[random.Next(services.Count)];
+        }
+
+        private Site GetRandomSite(Random random)
+        {
+            List<Site> sites = _context.Sites.ToList();
+
+            return sites[random.Next(sites.Count)];
+        }
+    }
+}
--- AnnuaireEntrepriseAPI/Program.cs
+++ AnnuaireEntrepriseAPI/Program.cs
@@ -1,17 +1,34 @@
+using AnnuaireEntrepriseAPI;
 using AnnuaireEntrepriseAPI.Database;
 using AnnuaireEntrepriseAPI.DTOs;
 using AnnuaireEntrepriseAPI.Models;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Insérer les 1000 salariés
+
 builder.Services.AddDbContext<AnnuaireEntrepriseContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerContext"));
 });
+
+//var optionsBuilder = new DbContextOptionsBuilder<AnnuaireEntrepriseContext>();
+//optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerContext"));
+
+//using (var dataContext = new AnnuaireEntrepriseContext(optionsBuilder.Options))
+//{
+//    var data = new DataGenerator(dataContext);
+//    data.GenerateData(dataContext);
+//}
+
+//Console.WriteLine("Insertion des données terminée !");
+//Console.ReadLine();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -34,3 +51,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
